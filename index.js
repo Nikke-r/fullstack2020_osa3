360c74c88c768d26db9b28d58eab32a8ed1baac9
Niklas Rönnholm <nikke.ronnholm@gmail.com>
2020-03-23 19:46:22 +0200

    .

--- index.js
+++ index.js
@@ -78,14 +78,14 @@ app.put('/api/persons/:id', (req, res, next) => {
         .catch(error => {
             next(error);
         });
-})
+});
 
 const errorHandler = (error, req, res, next) => {
 
     if (error.name === 'CastError' && error.kind === 'ObjectId') {
         return res.status(400).send({error: 'Malformatted ID'});
     } else if (error.name === 'ValidationError') {
-        return res.status(400).json({error: error});
+        return res.status(400).json({ error: error.message });
     }
 
     next(error);
