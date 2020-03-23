ebfe5c9f81e48eb69b8a22dcd0db625b91421883
Niklas Rönnholm <nikke.ronnholm@gmail.com>
2020-03-23 12:04:18 +0200

    frontend

---
+++ build/precache-manifest.2304d7157e89a363d535ae4c7e507a14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca51cd2b5112971a14f0232ce219605",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f15ebec07a8c2bebd38",
+    "url": "/static/css/main.eec699f7.chunk.css"
+  },
+  {
+    "revision": "859f324597064aadb412",
+    "url": "/static/js/2.21323808.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.21323808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f15ebec07a8c2bebd38",
+    "url": "/static/js/main.a54e7ad1.chunk.js"
+  },
+  {
+    "revision": "57e3d8805ca2bcfd1d13",
+    "url": "/static/js/runtime-main.fb40897c.js"
+  }
+]);
--- index.js
+++ index.js
@@ -16,6 +16,7 @@ morgan.token('body', (req, res) => {return JSON.stringify(req.body)});
 app.use(express.json());
 app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(express.static('build'));
 
 app.get('/api/persons', (req, res) => {
     res.json(persons);
